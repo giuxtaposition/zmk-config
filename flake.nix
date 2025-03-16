50b80e08ac3d1fb7794dd0750201475e10850d94
giuxtaposition <yg97.cs@gmail.com>
2025-03-16 16:14:24 +0100

    feat(sweep): update shield

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         src = nixpkgs.lib.sourceFilesBySuffices self [".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi" ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig"];
 
         board = "nice_nano_v2";
-        shield = "splitkb_aurora_sweep_%PART%";
+        shield = "cradio_%PART%";
 
         zephyrDepsHash = "sha256-luj5U1c5Rnb5unrk2LVnM10h+lzbgS65/XG261mtA4M=";
 
